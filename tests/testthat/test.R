7fd61a5ec6731a4d2cb7d63f57918d6dce063c0d
Jenny Bryan <jenny.f.bryan@gmail.com>
2018-12-02 09:21:31 -0800

    Reveal R_USER_* vars at check time

--- tests/testthat/test.R
+++ tests/testthat/test.R
@@ -6,3 +6,11 @@ test_that("I can use the secret powers", {
     rprojroot::find_testthat_root_file("supersecret")
   ))
 })
+
+test_that("R_USER_PROFILE is nonsense (force failure and reveal", {
+  expect_identical(Sys.getenv('R_USER_PROFILE'), "wtf")
+})
+
+test_that("R_USER_ENVIRON is nonsense (force failure and reveal", {
+  expect_identical(Sys.getenv('R_USER_ENVIRON'), "wtf")
+})
